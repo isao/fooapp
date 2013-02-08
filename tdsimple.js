8c47e955636e407a62627d760c7580925ff5a885
Isao Yagi <isao@yahoo-inc.com>
2013-02-08 10:37:25 -0800

    comment, output

--- tdsimple.js
+++ tdsimple.js
@@ -39,3 +39,56 @@ function locator(conf, callback) {
 locator(conf, function (err, out) {
     console.log('• config metadata', inspect(out, false, 8, true));
 });
+
+/*
+
+output:
+
+• scanned 34 items from ["tests/fixtures/touchdown-simple"]
+• config metadata { app: 
+   [ { settings: [ 'master' ],
+       selector: 'default' },
+     { settings: [ 'device:smartphone' ],
+       selector: 'smartphone' } ],
+  dimensions: 
+   [ { dimensions: 
+        [ { environment: 
+             { development: { dev: null },
+               integration: { int: null, int1: null },
+               qa: { qa1: null },
+               production: { prod: null, stage: null } } },
+          { device: 
+             { desktop: null,
+               mobile: 
+                { tablet: 
+                   { 'tablet-lite': null,
+                     'tablet-full': null },
+                  smartphone: 
+                   { 'smartphone-lite': null,
+                     'smartphone-full': null },
+                  featurephone: null },
+               tv: null,
+               console: null } },
+          { lang: 
+             { en: 
+                { 'en-GB': null,
+                  'en-US': null,
+                  'en-CA': null },
+               fr: { 'fr-FR': { 'fr-CA': null } } } },
+          { region: 
+             { US: null,
+               CA: null,
+               GB: null,
+               FR: null } } ] } ],
+  layouts: 
+   [ { settings: [ 'master' ],
+       default: { main: { type: 'foo' } } },
+     { settings: [ 'device:smartphone' ],
+       default: { main: { type: 'foo', config: { bar: 'baz' } } } } ],
+  config: 
+   [ { settings: [ 'master' ],
+       selector: 'default' },
+     { settings: [ 'device:smartphone' ],
+       selector: 'smartphone' } ] }
+
+*/
