91bee670ed257edb54c1e14571636a4dc0960ac8
Isao Yagi <isao@yahoo-inc.com>
2013-02-07 17:35:08 -0800

    copy fixtures

--- tests/fixtures-rs/store/mojits/test_mojit_1/models/test_1.server.js
+++
@@ -1,5 +0,0 @@
-YUI.add('test_mojit_1_model_test_1', function(Y, NAME){
-
-    Y.mojito.models.publications = {};
-
-}, '0.0.1', {requires: ['mojito']});
--- tests/fixtures-rs/store/mojits/test_mojit_1/models/test_2.server.js
+++
@@ -1,5 +0,0 @@
-YUI.add('test_mojit_1_model_test_2', function(Y, NAME){
-
-    Y.mojito.models.publications = {};
-
-}, '0.0.1', {requires: ['mojito']});
